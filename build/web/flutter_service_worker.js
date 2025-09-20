f991d3a51a7dab05c152abb5215bef25fe0b2d7b
John Mark M.Verdin <johnmark_verdin78@isufst.edu.ph>
2025-09-21 02:09:25 +0800

    Add build files for Netlify

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf3e65fdaaf0aa29e9c0da8feebf3042
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d7fec64445fd13b00e6386553add6620",
+"flutter_bootstrap.js": "a0b2853bc820020556108e32156d4b83",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
